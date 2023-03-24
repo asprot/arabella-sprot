02022602d4d13c9db5510cac9c02165a52f75bbd
asprot <128853936+asprot@users.noreply.github.com>
2023-03-24 20:44:38 +0000

    init commit

---
+++ mail.php
@@ -0,0 +1,32 @@
+
+
+<?php
+ 
+  if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response']))
+  {
+        $secret = '0xEa74500a88F693fE39dfe4e5c9867759fe877BCD';
+        $verifyResponse = file_get_contents('https://hcaptcha.com/siteverify?secret='.$secret.'&response='.$_POST['h-captcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success)
+        {
+            $succMsg = 'Your request have submitted successfully.';
+          
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "arabellasprot@gmail.com";
+$subject = "Website - Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+          echo 'success';
+   
+        }
+        else
+        {
+            $errMsg = 'Robot verification failed, please try again.';
+          echo 'fail';
+        }
+   }
+?>
