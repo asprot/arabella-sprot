a64b67205b58b1593def6293d782ce49f2c82b4d
asprot <128853936+asprot@users.noreply.github.com>
2025-05-25 13:48:25 +0100

    email

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "arabellasprot@gmail.com";
+$recipient = "arabella.sprot@gmail.com";
 $subject = "Website - Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
