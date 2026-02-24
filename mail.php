3d5662034f5a25719657d6fded4c3ee1d37cdb97
asprot <128853936+asprot@users.noreply.github.com>
2026-02-24 11:53:08 +0000

    edited form

--- mail.php
+++ mail.php
@@ -2,13 +2,13 @@
 
 <?php
  
- // if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response']))
- // {
-      // $secret = '0xEa74500a88F693fE39dfe4e5c9867759fe877BCD';
-      //  $verifyResponse = file_get_contents('https://hcaptcha.com/siteverify?secret='.$secret.'&response='.$_POST['h-captcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
-       // $responseData = json_decode($verifyResponse);
-      //if($responseData->success)
-      //  {
+  if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response']))
+  {
+       $secret = '0xEa74500a88F693fE39dfe4e5c9867759fe877BCD';
+        $verifyResponse = file_get_contents('https://hcaptcha.com/siteverify?secret='.$secret.'&response='.$_POST['h-captcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
+        $responseData = json_decode($verifyResponse);
+      if($responseData->success)
+        {
             $succMsg = 'Your request have submitted successfully.';
           
 
@@ -22,11 +22,11 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
           echo 'success';
    
-      //  }
-       // else
-      //  {
-        //    $errMsg = 'Robot verification failed, please try again.';
-       //   echo 'fail';
-       // }
-  // }
+        }
+        else
+        {
+            $errMsg = 'Robot verification failed, please try again.';
+          echo 'fail';
+        }
+   }
 ?>
